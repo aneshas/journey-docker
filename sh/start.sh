d9363601dda5f481cc42d19d43ae3624e60eecf1
Anes Hasicic <anes.hasicic@gmail.com>
2015-04-01 12:33:28 +0200

    Added .gitignore

--- sh/start.sh
+++ sh/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 go get github.com/kabukky/journey
 cd /root/go/src/github.com/kabukky/journey
-git submodule update --init --recursive
+#git submodule update --init --recursive
 go build
-./journey --custom-path /data
+./journey -custom-path="/data"
 while true; do sleep 1d; done
 
